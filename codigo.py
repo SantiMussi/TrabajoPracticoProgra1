b6d06752acb2a842433aa3d471eeb44fe26a6219
sanjuark <correagameroficial@gmail.com>
2023-11-15 17:34:18 -0300

    Codigo y modulos terminados

---
+++ codigo.py
@@ -0,0 +1,32 @@
+import math
+from modulo import *
+
+def escribirArchivo():
+    """La función se encarga de, en un archivo aparte, escribir
+    el título del juego, calificación del crítico, del usuario y además hacer un promedio"""
+    try:
+        with open("NintendoData.csv", "wt", encoding='UTF-8') as archivo:
+            lista_total = leerLineas("NintendoGames.csv", ",")
+            archivo.write(f"Juego; Calificacion Critico; Calificacion Usuario; Promedio\n")
+            
+            for lista in lista_total:
+                # Verificamos si las calificaciones existen
+                calificacion_critico = lista[0]
+                calificacion_usuario = lista[4]
+                promedio = 0
+                #Calculamos el promedio de las calificaciones
+                try:
+                    promedio = (float(calificacion_critico) + float(calificacion_usuario)) / 2
+                    promedio = int(promedio) if not math.isnan(promedio) else 0
+                except ValueError:
+                    pass
+                #Volcamos todo en el archivo nuevo
+                archivo.write(f"{lista[1]};{calificacion_critico};{calificacion_usuario};{promedio}\n")
+    except IOError:
+        print("No se pudo abrir el archivo de escritura")
+
+def main():
+    escribirArchivo()
+
+if __name__ == "__main__":
+    main()
---
+++ modulo.py
@@ -0,0 +1,34 @@
+import math
+def leerLineas(ubicacion, separador):
+    """La función se encarga de realizar una lectura de las líneas 
+    y dividirlas con el separador indicado en los parámetros, además
+    tiene la ubicación colocada en los parámetros para que sea
+    reutilizable"""
+    try: 
+        lista_total = []
+        # Abrimos el archivo en modo lectura
+        with open(ubicacion, 'rt', encoding='UTF-8') as archivo:
+            next(archivo)
+            #Chequeamos si las columnas que contienen "" tienen comas adentro para que no se corrompa el codigo
+            for linea in archivo:
+                lista = []
+                #Bandera para ver si estamos en un bloque dentro de comillas
+                dentro_comillas = False
+                partes = linea.strip().split(separador)
+                for parte in partes:
+                    if parte.startswith('"'):
+                        dentro_comillas = True
+                        columna = parte
+                    elif parte.endswith('"'):
+                        dentro_comillas = False
+                        columna += separador + parte
+                        lista.append(columna[1:-1])
+                    elif dentro_comillas:
+                        columna += separador + parte
+                    else:
+                        lista.append(parte)
+                lista_total.append(lista)
+    except IOError:
+        print("No se pudo abrir el archivo de lectura de la ubicación")
+    return lista_total
+
