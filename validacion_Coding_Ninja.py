16c45781c4e46aedd72fa62ff38747319c29b0a4
Santiago Mussi <66830160+SantiMussi@users.noreply.github.com>
2023-11-16 07:39:06 -0300

    Agregado el metodo de validación de otro equipo

---
+++ validacion_Coding_Ninja.py
@@ -0,0 +1,7 @@
+def validacion(usuario1, contraseña1):
+  usuario = "Coding_Ninja"
+  contraseña = "Minecraft123"
+  resultado = False
+  if (usuario1 == usuario) and (contraseña == contraseña1):
+    resultado = True
+  return resultado
